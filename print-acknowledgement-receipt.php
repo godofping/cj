1d158635d911a2f1cf0261de8d1d24f18fdf644c
godofping <rexmicrosoft@yahoo.com.ph>
2019-04-19 15:27:54 +0800

    Update print-acknowledgement-receipt.php

--- print-acknowledgement-receipt.php
+++ print-acknowledgement-receipt.php
@@ -83,6 +83,8 @@ html {margin-top: 60px}
 <section class=" padding-bottom-100">
   <div class="container">
 
+      <div style="text-align: center; margin-bottom: 20px;"><img src="http://cjashleyfashionhub.tk/dashboard/etc/logo.jpg" height= "100px" /></div>
+
       <div class="row">
      	<div class="col-md-12">
      		<h4 class="text-center">Acknowledgement Receipt</h4>
